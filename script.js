3a9905ef221fe4037b2adac611d8c74cefce0c77
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-09-01 12:06:01 -0400

    fixed password length

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ function generatePassword() {
   var chars = Numeric + UpperLower + SpecialChars
 
   var password = "";
-  for (var i = 0; i <= passwordLength; i++) {
+  for (var i = 0; i < passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber +1);
    }
